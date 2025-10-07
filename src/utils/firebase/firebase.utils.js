8773ed028541e234355feeba50c205084cd67f36
BiggerThings <nnsbiggerthings@gmail.com>
2025-10-07 21:49:05 +0200

    Redux Toolkit: Updated firebase config to my own

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyCe7kIXe1LaXgkyBJp1zImqxVM0SI84QcY",
+  authDomain: "crwn-clothing-db-50e39.firebaseapp.com",
+  projectId: "crwn-clothing-db-50e39",
+  storageBucket: "crwn-clothing-db-50e39.firebasestorage.app",
+  messagingSenderId: "834057447281",
+  appId: "1:834057447281:web:ac45f245d52f0ff5d1b8d0"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
