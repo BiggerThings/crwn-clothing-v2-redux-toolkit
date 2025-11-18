7ae04dca8e2403d55b8f149081b2d94dea4f6489
BiggerThings <nnsbiggerthings@gmail.com>
2025-11-18 22:36:28 +0200

    Netlify: Set up stripe sandbox backend in Netlify's serverless functions

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -15,11 +15,37 @@ const PaymentForm = () => {
             return;
         }
 
+        const response = await fetch("/.netlify/functions/create-payment-intent", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then(res => res.json());
+
+        const { paymentIntent: { client_secret } } = response;
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: "Ntate Mapena"
+                }
+            }
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error.message);
+        } else {
+            if (paymentResult.paymentIntent.status === "succeeded") {
+                alert("Payment Successful!");
+            }
+        }
     }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment:</h2>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
