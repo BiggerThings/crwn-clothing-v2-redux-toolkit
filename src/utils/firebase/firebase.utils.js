0ad351708320cc5e60a641980855b5c02ca37c1f
BiggerThings <nnsbiggerthings@gmail.com>
2025-12-09 22:18:50 +0200

    Netflify deployment: exported firebaseApp from firebase utils to mitigate netlify deploy error

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -29,7 +29,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseApp = initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
