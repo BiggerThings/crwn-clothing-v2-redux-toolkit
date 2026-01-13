7cb11f729dd0aae81f704ca51d1c51f494d0af52
BiggerThings <nnsbiggerthings@gmail.com>
2026-01-13 21:27:28 +0200

    TS: Typed state as n any type in category reducer

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -2,7 +2,7 @@ import { createSelector } from 'reselect';
 
 import { CategoriesState, CategoryMap } from './category.reducer';
 
-const selectCategoryReducer = (state): CategoriesState => state.categories;
+const selectCategoryReducer = (state: any): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
