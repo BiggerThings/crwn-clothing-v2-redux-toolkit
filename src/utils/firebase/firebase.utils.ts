fa4ccb532d6b6cbd4329a5674ffc240e9cb2adad
BiggerThings <nnsbiggerthings@gmail.com>
2026-03-05 21:56:49 +0200

    TS: Typed Sign-Up Component

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -118,7 +118,6 @@ export const createUserDocumentFromAuth = async (
 };
 
 export const createAuthUserWithEmailAndPassword = async (email: string, password: string) => {
-  if (!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
 };
